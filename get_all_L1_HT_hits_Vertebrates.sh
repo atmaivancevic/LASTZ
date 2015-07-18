8a17d195432c0b1b6b66ff8ca74578f86834f98f
Atma Ivancevic <atma.ivancevic@adelaide.edu.au>
2015-07-18 12:08:59 +0000

    updated

--- get_all_L1_HT_hits_Vertebrates.sh
+++ get_all_L1_HT_hits_Vertebrates.sh
@@ -7,103 +7,103 @@
 # Vertebrates
 
 # # Low-seq-count genomes (<10,000 seqs)
-./screen_genome_for_HT_candidate.sh Opossum 1 10
-./screen_genome_for_HT_candidate.sh Stickleback 1 24
-./screen_genome_for_HT_candidate.sh ZebraFinch 1 35
-./screen_genome_for_HT_candidate.sh Horse 1 36
-./screen_genome_for_HT_candidate.sh Mouse 1 67
-./screen_genome_for_HT_candidate.sh hg19 1 84
-./screen_genome_for_HT_candidate.sh Human 1 195
-./screen_genome_for_HT_candidate.sh Zebrafish 1 1135
-./screen_genome_for_HT_candidate.sh SiberianTiger 1 1480
-./screen_genome_for_HT_candidate.sh KillerWhale 1 1669
-./screen_genome_for_HT_candidate.sh SpottedGar 1 1927
-./screen_genome_for_HT_candidate.sh StarnosedMole 1 2042
-./screen_genome_for_HT_candidate.sh Elephant 1 2305
-./screen_genome_for_HT_candidate.sh SquirrelMonkey 1 2687
-./screen_genome_for_HT_candidate.sh Rat 1 2740
-./screen_genome_for_HT_candidate.sh Chinchilla 1 2840 
-./screen_genome_for_HT_candidate.sh WhiteRhino 1 3088
-./screen_genome_for_HT_candidate.sh GuineaPig 1 3146
-./screen_genome_for_HT_candidate.sh Rabbit 1 3244
-./screen_genome_for_HT_candidate.sh Dog 1 3269
-./screen_genome_for_HT_candidate.sh Cow 1 3319 
-./screen_genome_for_HT_candidate.sh ZebraMbuna 1 3727
-./screen_genome_for_HT_candidate.sh Walrus 1 3894
-./screen_genome_for_HT_candidate.sh AmazonMolly 1 3987 
-./screen_genome_for_HT_candidate.sh NakedMolerat 1 4230
-./screen_genome_for_HT_candidate.sh Pig 1 4584
-./screen_genome_for_HT_candidate.sh GroundTit 1 5407
-./screen_genome_for_HT_candidate.sh Cat 1 5502 
-./screen_genome_for_HT_candidate.sh NileTilapia 1 5680
-./screen_genome_for_HT_candidate.sh Sheep 1 5700
-./screen_genome_for_HT_candidate.sh Hedgehog 1 5804
-./screen_genome_for_HT_candidate.sh SakerFalcon 1 5865
-./screen_genome_for_HT_candidate.sh JapaneseQuail 1 5878
-./screen_genome_for_HT_candidate.sh Turkey 1 5893
-./screen_genome_for_HT_candidate.sh WhitethroatedSparrow 1 6020
-./screen_genome_for_HT_candidate.sh Manatee 1 6324
-./screen_genome_for_HT_candidate.sh PrairieVole 1 6337
-./screen_genome_for_HT_candidate.sh Lizard 1 6459
-./screen_genome_for_HT_candidate.sh BigBrownBat 1 6791
-./screen_genome_for_HT_candidate.sh Fugu 1 6837
-./screen_genome_for_HT_candidate.sh PeregrineFalcon 1 7022
-./screen_genome_for_HT_candidate.sh Degu 1 7136
-./screen_genome_for_HT_candidate.sh Medaka 1 7190
-./screen_genome_for_HT_candidate.sh FlameBackCichlid 1 7238
-./screen_genome_for_HT_candidate.sh CrabeatingMacaque 1 7602
-./screen_genome_for_HT_candidate.sh GreenMonkey 1 7612
-./screen_genome_for_HT_candidate.sh WesternClawedFrog 1 7729
-./screen_genome_for_HT_candidate.sh Ferret 1 7784
-./screen_genome_for_HT_candidate.sh Bushbaby 1 7795
-./screen_genome_for_HT_candidate.sh AstatotilapiaBurtoni 1 8003 
-./screen_genome_for_HT_candidate.sh Tenrec 1 8403
-./screen_genome_for_HT_candidate.sh SeaHare 1 8768
-./screen_genome_for_HT_candidate.sh CapeElephantShrew 1 8770
-./screen_genome_for_HT_candidate.sh NeolamprologusBrichardi 1 9100
-./screen_genome_for_HT_candidate.sh ChineseAlligator 1 9318
+# ./screen_genome_for_HT_candidate.sh Opossum 1 10
+# ./screen_genome_for_HT_candidate.sh Stickleback 1 24
+# ./screen_genome_for_HT_candidate.sh ZebraFinch 1 35
+# ./screen_genome_for_HT_candidate.sh Horse 1 36
+# ./screen_genome_for_HT_candidate.sh Mouse 1 67
+# ./screen_genome_for_HT_candidate.sh hg19 1 84
+# ./screen_genome_for_HT_candidate.sh Human 1 195
+# ./screen_genome_for_HT_candidate.sh Zebrafish 1 1135
+# ./screen_genome_for_HT_candidate.sh SiberianTiger 1 1480
+# ./screen_genome_for_HT_candidate.sh KillerWhale 1 1669
+# ./screen_genome_for_HT_candidate.sh SpottedGar 1 1927
+# ./screen_genome_for_HT_candidate.sh StarnosedMole 1 2042
+# ./screen_genome_for_HT_candidate.sh Elephant 1 2305
+# ./screen_genome_for_HT_candidate.sh SquirrelMonkey 1 2687
+# ./screen_genome_for_HT_candidate.sh Rat 1 2740
+# ./screen_genome_for_HT_candidate.sh Chinchilla 1 2840 
+# ./screen_genome_for_HT_candidate.sh WhiteRhino 1 3088
+# ./screen_genome_for_HT_candidate.sh GuineaPig 1 3146
+# ./screen_genome_for_HT_candidate.sh Rabbit 1 3244
+# ./screen_genome_for_HT_candidate.sh Dog 1 3269
+# ./screen_genome_for_HT_candidate.sh Cow 1 3319 
+# ./screen_genome_for_HT_candidate.sh ZebraMbuna 1 3727
+# ./screen_genome_for_HT_candidate.sh Walrus 1 3894
+# ./screen_genome_for_HT_candidate.sh AmazonMolly 1 3987 
+# ./screen_genome_for_HT_candidate.sh NakedMolerat 1 4230
+# ./screen_genome_for_HT_candidate.sh Pig 1 4584
+# ./screen_genome_for_HT_candidate.sh GroundTit 1 5407
+# ./screen_genome_for_HT_candidate.sh Cat 1 5502 
+# ./screen_genome_for_HT_candidate.sh NileTilapia 1 5680
+# ./screen_genome_for_HT_candidate.sh Sheep 1 5700
+# ./screen_genome_for_HT_candidate.sh Hedgehog 1 5804
+# ./screen_genome_for_HT_candidate.sh SakerFalcon 1 5865
+# ./screen_genome_for_HT_candidate.sh JapaneseQuail 1 5878
+# ./screen_genome_for_HT_candidate.sh Turkey 1 5893
+# ./screen_genome_for_HT_candidate.sh WhitethroatedSparrow 1 6020
+# ./screen_genome_for_HT_candidate.sh Manatee 1 6324
+# ./screen_genome_for_HT_candidate.sh PrairieVole 1 6337
+# ./screen_genome_for_HT_candidate.sh Lizard 1 6459
+# ./screen_genome_for_HT_candidate.sh BigBrownBat 1 6791
+# ./screen_genome_for_HT_candidate.sh Fugu 1 6837
+# ./screen_genome_for_HT_candidate.sh PeregrineFalcon 1 7022
+# ./screen_genome_for_HT_candidate.sh Degu 1 7136
+# ./screen_genome_for_HT_candidate.sh Medaka 1 7190
+# ./screen_genome_for_HT_candidate.sh FlameBackCichlid 1 7238
+# ./screen_genome_for_HT_candidate.sh CrabeatingMacaque 1 7602
+# ./screen_genome_for_HT_candidate.sh GreenMonkey 1 7612
+# ./screen_genome_for_HT_candidate.sh WesternClawedFrog 1 7729
+# ./screen_genome_for_HT_candidate.sh Ferret 1 7784
+# ./screen_genome_for_HT_candidate.sh Bushbaby 1 7795
+# ./screen_genome_for_HT_candidate.sh AstatotilapiaBurtoni 1 8003 
+# ./screen_genome_for_HT_candidate.sh Tenrec 1 8403
+# ./screen_genome_for_HT_candidate.sh SeaHare 1 8768
+# ./screen_genome_for_HT_candidate.sh CapeElephantShrew 1 8770
+# ./screen_genome_for_HT_candidate.sh NeolamprologusBrichardi 1 9100
+# ./screen_genome_for_HT_candidate.sh ChineseAlligator 1 9318
 
 # # Medium-seq-count genomes (>10,000 but <100,000 seqs)
-./screen_genome_for_HT_candidate.sh AmericanPika 1 10422 
-./screen_genome_for_HT_candidate.sh MexicanTetra 1 10737
-./screen_genome_for_HT_candidate.sh MinkeWhale 1 10777
-./screen_genome_for_HT_candidate.sh Bonobo 1 10868
-./screen_genome_for_HT_candidate.sh LesserEgyptianJerboa 1 10899
-./screen_genome_for_HT_candidate.sh Microbat 1 11656
-./screen_genome_for_HT_candidate.sh SpermWhale 1 11712
-./screen_genome_for_HT_candidate.sh KangarooRat 1 11852
-./screen_genome_for_HT_candidate.sh Squirrel 1 12485
-./screen_genome_for_HT_candidate.sh CommonShrew 1 12847 
-./screen_genome_for_HT_candidate.sh BactrianCamel 1 13335
-./screen_genome_for_HT_candidate.sh Marmoset 1 14207
-./screen_genome_for_HT_candidate.sh AmericanAlligator 1 14646
-./screen_genome_for_HT_candidate.sh RockDove 1 14924
-./screen_genome_for_HT_candidate.sh TibetanAntelope 1 15060
-./screen_genome_for_HT_candidate.sh Chicken 1 15934
-./screen_genome_for_HT_candidate.sh WeddellSeal 1 16712
-./screen_genome_for_HT_candidate.sh Gibbon 1 17494
-./screen_genome_for_HT_candidate.sh ChineseSoftshellTurtle 1 19905
-./screen_genome_for_HT_candidate.sh CapeGoldenMole 1 20501
-./screen_genome_for_HT_candidate.sh SouthernPlatyfish 1 20633
-./screen_genome_for_HT_candidate.sh ElephantShark 1 21206
-./screen_genome_for_HT_candidate.sh CollaredFlycatcher 1 21429
-./screen_genome_for_HT_candidate.sh GoldenHamster 1 21485
-./screen_genome_for_HT_candidate.sh Aardvark 1 22510
-./screen_genome_for_HT_candidate.sh Coelacanth 1 22820
-./screen_genome_for_HT_candidate.sh Chimp 1 24130
-./screen_genome_for_HT_candidate.sh Lamprey 1 25007
-./screen_genome_for_HT_candidate.sh Budgerigar 1 25214
-./screen_genome_for_HT_candidate.sh MediumGroundfinch 1 27241
-./screen_genome_for_HT_candidate.sh Baiji 1 30714
-./screen_genome_for_HT_candidate.sh DeerMouse 1 30923
-./screen_genome_for_HT_candidate.sh C_semi 1 31204
-./screen_genome_for_HT_candidate.sh Rhesus 1 34105
-./screen_genome_for_HT_candidate.sh YellowbellyPufferfish 1 3916
-./screen_genome_for_HT_candidate.sh TasDevil 1 14149
-./screen_genome_for_HT_candidate.sh BurmesePython 1 12997
-./screen_genome_for_HT_candidate.sh Yak 1 5060
-./screen_genome_for_HT_candidate.sh Armadillo 1 21713
-./screen_genome_for_HT_candidate.sh Gorilla 1 46824
+# ./screen_genome_for_HT_candidate.sh AmericanPika 1 10422 
+# ./screen_genome_for_HT_candidate.sh MexicanTetra 1 10737
+# ./screen_genome_for_HT_candidate.sh MinkeWhale 1 10777
+# ./screen_genome_for_HT_candidate.sh Bonobo 1 10868
+# ./screen_genome_for_HT_candidate.sh LesserEgyptianJerboa 1 10899
+# ./screen_genome_for_HT_candidate.sh Microbat 1 11656
+# ./screen_genome_for_HT_candidate.sh SpermWhale 1 11712
+# ./screen_genome_for_HT_candidate.sh KangarooRat 1 11852
+# ./screen_genome_for_HT_candidate.sh Squirrel 1 12485
+# ./screen_genome_for_HT_candidate.sh CommonShrew 1 12847 
+# ./screen_genome_for_HT_candidate.sh BactrianCamel 1 13335
+# ./screen_genome_for_HT_candidate.sh Marmoset 1 14207
+# ./screen_genome_for_HT_candidate.sh AmericanAlligator 1 14646
+# ./screen_genome_for_HT_candidate.sh RockDove 1 14924
+# ./screen_genome_for_HT_candidate.sh TibetanAntelope 1 15060
+# ./screen_genome_for_HT_candidate.sh Chicken 1 15934
+# ./screen_genome_for_HT_candidate.sh WeddellSeal 1 16712
+# ./screen_genome_for_HT_candidate.sh Gibbon 1 17494
+# ./screen_genome_for_HT_candidate.sh ChineseSoftshellTurtle 1 19905
+# ./screen_genome_for_HT_candidate.sh CapeGoldenMole 1 20501
+# ./screen_genome_for_HT_candidate.sh SouthernPlatyfish 1 20633
+# ./screen_genome_for_HT_candidate.sh ElephantShark 1 21206
+# ./screen_genome_for_HT_candidate.sh CollaredFlycatcher 1 21429
+# ./screen_genome_for_HT_candidate.sh GoldenHamster 1 21485
+# ./screen_genome_for_HT_candidate.sh Aardvark 1 22510
+# ./screen_genome_for_HT_candidate.sh Coelacanth 1 22820
+# ./screen_genome_for_HT_candidate.sh Chimp 1 24130
+# ./screen_genome_for_HT_candidate.sh Lamprey 1 25007
+# ./screen_genome_for_HT_candidate.sh Budgerigar 1 25214
+# ./screen_genome_for_HT_candidate.sh MediumGroundfinch 1 27241
+# ./screen_genome_for_HT_candidate.sh Baiji 1 30714
+# ./screen_genome_for_HT_candidate.sh DeerMouse 1 30923
+# ./screen_genome_for_HT_candidate.sh C_semi 1 31204
+# ./screen_genome_for_HT_candidate.sh Rhesus 1 34105
+# ./screen_genome_for_HT_candidate.sh YellowbellyPufferfish 1 3916
+# ./screen_genome_for_HT_candidate.sh TasDevil 1 14149
+# ./screen_genome_for_HT_candidate.sh BurmesePython 1 12997
+# ./screen_genome_for_HT_candidate.sh Yak 1 5060
+# ./screen_genome_for_HT_candidate.sh Armadillo 1 21713
+# ./screen_genome_for_HT_candidate.sh Gorilla 1 46824
 ./screen_genome_for_HT_candidate.sh ChineseTreeShrew 1 50752
 ./screen_genome_for_HT_candidate.sh SumatranOrangutan 1 61537
 ./screen_genome_for_HT_candidate.sh Baboon 1 63252
